7682928aa1d9bf0eda9e256bd36d05429cc2d344
Danil <ferrum268@mail.ru>
2023-04-19 18:56:41 +0500

    Create a whole Puthon project

--- interpreter.py
+++ interpreter.py
@@ -1,12 +1,28 @@
 import sys
+import os
 
 
 def main():
+    if not is_path_correct():
+        print("ERROR")
+        return
+
+
+def is_path_correct():
+    extensions = [".be", ".bf", ".b93", ".b98", ".befunge"]
     if len(sys.argv) != 2:
-        print("Usage:"
-              "python ")
+        print(f"Usage:\n"
+              f"\tpython interpreter.py <absolute path to befunge file>\n"
+              f"Extensions: {extensions}")
+    elif not os.path.isfile(sys.argv[1]):
+        print(f"Could not find {sys.argv[1]}")
+    elif os.path.splitext(sys.argv[1])[1] not in extensions:
+        print(f"{sys.argv[1]} does not have a valid extension")
+    else:
+        print(f"Filename is correct. Interpreting {os.path.basename(sys.argv[1])}...")
+        return True
+    return False
 
 
 if __name__ == "__main__":
-    print("xD")
     main()
