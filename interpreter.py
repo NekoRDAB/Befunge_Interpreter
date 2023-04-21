667052260820f75f91e475ba141787f2fe1780e1
Danil <ferrum268@mail.ru>
2023-04-21 13:32:45 +0500

    add Interpreter class

--- interpreter.py
+++ interpreter.py
@@ -1,28 +1,7 @@
-import sys
-import os
+class Interpreter:
+    def __init__(self, path):
+        self.code = self.read_code(path)
 
-
-def main():
-    if not is_path_correct():
-        print("ERROR")
-        return
-
-
-def is_path_correct():
-    extensions = [".be", ".bf", ".b93", ".b98", ".befunge"]
-    if len(sys.argv) != 2:
-        print(f"Usage:\n"
-              f"\tpython interpreter.py <absolute path to befunge file>\n"
-              f"Extensions: {extensions}")
-    elif not os.path.isfile(sys.argv[1]):
-        print(f"Could not find {sys.argv[1]}")
-    elif os.path.splitext(sys.argv[1])[1] not in extensions:
-        print(f"{sys.argv[1]} does not have a valid extension")
-    else:
-        print(f"Filename is correct. Interpreting {os.path.basename(sys.argv[1])}...")
-        return True
-    return False
-
-
-if __name__ == "__main__":
-    main()
+    def read_code(self, path):
+        with open(path) as file:
+            return [line for line in file.readlines()]
---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from interpreter import Interpreter
+
+
+def main():
+    if not arguments_correct():
+        return
+    path = sys.argv[1]
+    interpreter = Interpreter(path)
+
+
+def arguments_correct():
+    extensions = [".be", ".bf", ".b93", ".b98", ".befunge"]
+    if len(sys.argv) != 2:
+        print(f"Usage:\n"
+              f"\tpython interpreter.py <absolute path to befunge file>\n"
+              f"Extensions: {extensions}")
+    elif not os.path.isfile(sys.argv[1]):
+        print(f"Could not find {sys.argv[1]}")
+    elif os.path.splitext(sys.argv[1])[-1] not in extensions:
+        print(f"{sys.argv[1]} does not have a valid extension")
+    else:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    main()
