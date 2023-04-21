596bf3d281e2c4a3a2d9bb9abc7ef751e7815e11
Danil <ferrum268@mail.ru>
2023-04-21 14:29:25 +0500

    begin some methods

--- interpreter.py
+++ interpreter.py
@@ -7,18 +7,27 @@ class Interpreter:
         self.height = 0
         self.code = self.read_code_from_file(path)
         self.pointer = Pointer(self.width, self.height)
-
+        self.instructions = dict()
 
     def run(self):
         while self.pointer.is_inside():
-            # TODO: finish later
-            pass
+            instruction = self.get_current_instruction()
 
     def read_code_from_file(self, path):
         with open(path) as file:
-            result = []
+            lines = []
             for line in file.readlines():
-                result.append(line)
+                lines.append(line)
                 self.height += 1
                 self.width = max(self.width, len(line))
+            result = []
+            for line in lines:
+                result.append(line + ' ' * (self.width - len(line)))
             return result
+
+    def get_current_instruction(self):
+        x, y = self.pointer.get_position()
+        return self.code[y][x]
+
+    def add_base_instructions(self):
+        self.instructions['>'] = lambda:
--- pointer.py
+++ pointer.py
@@ -4,6 +4,14 @@ class Pointer:
         self.y = 0
         self.width = width
         self.height = height
+        self.direction = None
 
     def is_inside(self):
         return 0 <= self.x < self.width and 0 <= self.y < self.height
+
+    def get_position(self):
+        return self.x, self.y
+
+    def change_direction(self, ):
+
+    def move(self):
