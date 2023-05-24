eb6a489a858ced12e6b4ee1250768c20e8e934e0
Danil <ferrum268@mail.ru>
2023-05-24 17:16:02 +0500

    create funge space and add tests for it

---
+++ Tests/befunge98_tests/funge_space_tests.py
@@ -0,0 +1,54 @@
+import unittest
+from funge_space import FungeSpace
+
+
+PATH_TO_FILE = (r"C:\Users\Professional\Desktop\python_files"
+               r"\pythonProject\Befunge_Interpreter"
+               r"\Tests\befunge98_tests\test_files\example_file.befunge")
+WIDTH = 17
+HEIGHT = 4
+
+
+class Test(unittest.TestCase):
+    def test_init(self):
+        with open(PATH_TO_FILE) as file:
+            space = FungeSpace(file)
+            self.assertEqual((WIDTH, HEIGHT), (space.width, space.height))
+
+    def test_in_space_get(self):
+        with open(PATH_TO_FILE) as file:
+            space = FungeSpace(file)
+            self.assertEqual('>', space[0, 0])
+            self.assertEqual('l', space[5, 1])
+            self.assertEqual(' ', space[16, 3])
+            self.assertEqual((WIDTH, HEIGHT), (space.width, space.height))
+
+    def test_out_of_space_get(self):
+        with open(PATH_TO_FILE) as file:
+            space = FungeSpace(file)
+            self.assertEqual(' ', space[17, 0])
+            self.assertEqual((18, HEIGHT), (space.width, space.height))
+            self.assertEqual(' ', space[150, 150])
+            self.assertEqual((151, 151), (space.width, space.height))
+
+    def test_in_space_set(self):
+        with open(PATH_TO_FILE) as file:
+            space = FungeSpace(file)
+            space[0, 0] = 'D'
+            space[16, 3] = '%'
+            self.assertEqual('D', space[0, 0])
+            self.assertEqual('%', space[16, 3])
+
+    def test_out_of_space_set(self):
+        with open(PATH_TO_FILE) as file:
+            space = FungeSpace(file)
+            space[17, 4] = "D"
+            self.assertEqual('D', space[17, 4])
+            self.assertEqual((18, 5), (space.width, space.height))
+            space[150, 150] = "Y"
+            self.assertEqual('Y', space[150, 150])
+            self.assertEqual((151, 151), (space.width, space.height))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- funge_space.py
+++ funge_space.py
@@ -1,2 +1,37 @@
 class FungeSpace:
     def __init__(self, file):
+        lines = []
+        self.width = 0
+        self.height = 0
+        self.code = []
+        for line in file.readlines():
+            lines.append(line)
+            self.width = max(self.width, len(line))
+            self.height += 1
+
+        for line in lines:
+            self.code.append(line + ' ' * (self.width - len(line)))
+
+    def extend_space(self, x, y):
+        self.width = max(x + 1, self.width)
+        offset = self.width - len(self.code[0])
+        for i in range(self.height):
+            self.code[i] += ' ' * offset
+        for i in range(self.height, x + 1):
+            self.code.append(' ' * self.width)
+        self.height = max(y + 1, self.height)
+
+    def out_of_space(self, x, y):
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def __getitem__(self, index):
+        x, y = index
+        self.extend_space(x, y)
+        return self.code[y][x]
+
+    def __setitem__(self, key, value):
+        if (type(value) != str) or len(value) != 1:
+            raise ValueError("expected string of length 1, actual is", value)
+        x, y = key
+        self.extend_space(x, y)
+        self.code[y] = self.code[y][:x] + value + self.code[y][x+1:]
